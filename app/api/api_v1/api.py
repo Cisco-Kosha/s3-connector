440f4daecd14a73b636f2c57c74d7d407be83966
Pavan Sudheendra <pavan0591@gmail.com>
2022-06-11 12:19:02 +0100

    remove /s3 prefix from api endpoints

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -4,4 +4,4 @@ from app.api.api_v1.endpoints import buckets
 
 api_router = APIRouter()
 
-api_router.include_router(buckets.router, prefix="/s3/buckets", tags=["buckets"])
+api_router.include_router(buckets.router, prefix="/buckets", tags=["buckets"])
